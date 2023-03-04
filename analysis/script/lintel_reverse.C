949f3415c55d5ca00f3dcfb0fa7154b7c85ce8fe
Prakash <fku2vr@virginia.edu>
2023-03-04 02:56:29 -0500

    Refactor utils and update lintel_reverse

--- analysis/script/lintel_reverse.C
+++ analysis/script/lintel_reverse.C
@@ -15,20 +15,9 @@ void get_lintel_coordinates(std::vector<double>& x, std::vector<double>& y, char
     }
 }
 
-bool md_ring_cut(RemollHit hit, int ring = 0){
-    int rmin = 0; int rmax = 0; int offset = 10; // Recheck offset
-    if(ring == 0){ rmin = 650; rmax = 1160; } // ring = 0 means no ring cut
-    else if(ring == 1) {rmin = 650; rmax = 680;} // Table from  DocDB 896-v1
-    else if(ring == 2) {rmin = 680; rmax = 740; }
-    else if(ring == 3) {rmin = 740; rmax = 800; }
-    else if(ring == 4) {rmin = 800; rmax = 920; }
-    else if(ring == 5) {rmin = 920; rmax = 1060; }
-    else if(ring == 6) {rmin = 1060; rmax = 1160; }
-    return (hit.r > rmin+offset) && (hit.r <= rmax+offset);
-}
 
 bool lintel_pos_cut(RemollHit hit){
-    return hit.x > -805 && hit.x < -150 && hit.y > 0.1 && hit.y < 150;
+    return hit.x > -805 && hit.x < -150 &&  0.0 <= hit.y  && hit.y <= 150;
 }
 
 /* reverse_lookup: Given a list of hits, select those hits with `hit.det == from`,
@@ -43,7 +32,7 @@ hit_list reverse_lookup(hit_list hits,int from, int to,int ring=0) {
     std::vector<int> trids;
     std::vector<RemollHit>  rev_hits(0);
     for(auto hit: hits){
-        if(hit.det == from &&  md_ring_cut(hit,ring) ){
+        if(hit.det == from &&  utl::md_ring_cut(hit,ring) ){
             trids.push_back(hit.trid);
         }
     }
@@ -69,27 +58,65 @@ std::pair<double,double> get_xy(RemollHit hit) {
     return std::pair<double,double>{hit.x,hit.y};
 }
 
+void update_histogram(TH2D* hist,double xmin,double xmax,double ymin,double ymax,double vmin,double vmax){
+    hist->SetStats(kFALSE);
+    //hist->SetAxisRange(vmin, vmax, "Z");
+    //hist->SetAxisRange(xmin, xmax, "X");
+    //hist->SetAxisRange(ymin, ymax, "Y");
+    hist->GetXaxis()->SetTitle("x [mm]");
+    hist->GetYaxis()->SetTitle("y [mm]");
+    hist->Draw("colz");
+}
+
+double fill_hist2d(TH2D* hist2d, pair_func get_xy, TTree* T, std::function<hit_list(hit_list)> lookup, std::function<bool(RemollHit)> cut, bool rateweight=false) {
+    hit_list  *hits=0; double rate;
+    T->SetBranchAddress("hit",&hits);
+    T->SetBranchAddress("rate",&rate);
+    int totentry = T->GetEntries();
+    double calc_rate = 0;
+    for(int entry = 0; entry <= totentry; ++entry) {
+        T->GetEntry(entry);
+        //if(rate > 1e10) continue; // seems to me that 1e11 is the bogus rate value.
+        hit_list rev_hits = lookup(*hits);
+        if(!rateweight) rate = 1;
+        bool flag = false;
+        for(RemollHit hit: rev_hits ) {
+            auto xyp = get_xy(hit);
+            double x = xyp.first; double y = xyp.second;
+            if( cut(hit) ) {
+                flag = true;
+                calc_rate += rate;
+                hist2d->Fill(x,y,rate);
+            }
+        }
+        //if(flag) calc_rate += rate;
+    }
+    return calc_rate;
+}
+
 void lintel_reverse(std::string filelist,int detid,int ring = 0, std::string weight="yes",  std::string strategy="normal", std::string id="default") {
     TChain* lintels = utl::make_chain(filelist);
+    int nof = lintels->GetListOfFiles()->GetSize();
     TCanvas* c1 = new TCanvas("c1","c1");
+    c1->SetFillColor(TColor::GetColor(190,190,190));
     std::string title = detid == 65 ? "Lintel Entry" : "Lintel Exit";
     if(strategy == "reverse") {title += ring == 5 ? " -> Ring 5 at MD " : " -> All Rings at MD";}
     std::cout<<"title :"<<title<<std::endl;
     TH2D* xyhist = new TH2D("Lintel",title.c_str(),120,-850,-150,120,0,150);
-    auto electron_at_lintel = [&](RemollHit h)->bool{return utl::det_pid_cut(h,detid,PID::ELECTRON) && lintel_pos_cut(h) ;};
+    //TH2D* xyhist = new TH2D("Lintel",title.c_str(),810,-850,850,810,-850,850);
+    auto electron_at_lintel = [&](RemollHit h)->bool{return utl::det_pid_cut(h,detid,PID::ELECTRON);};// && lintel_pos_cut(h) ;};
     typedef std::function<hit_list(hit_list)> lookup_func; // hit_list is a typedef for std::vector<RemollHit>
     lookup_func lintel_to_md = [&](hit_list hl)->hit_list{return reverse_lookup(hl,DETID::MD,detid,ring);};
     lookup_func all_events = [&](hit_list hl)->hit_list{return hl;};
     lookup_func lookup =  strategy == "normal" ? all_events : lintel_to_md;
-    double integral = utl::fill_hist2d(xyhist,get_xy,lintels,lookup,electron_at_lintel,weight=="yes");
-    std::cout<<"Rate :: "<<integral<<std::endl;
-    xyhist->SetStats(kFALSE);
-    xyhist->Draw("colz");
-    utl::show_text(Form("Rate = %0.3e ",integral),0.8,0.85,0.03);
-    xyhist->GetXaxis()->SetTitle("x [mm]");
-    xyhist->GetYaxis()->SetTitle("y [mm]");
-    add_lintel_outline(c1,detid == 65 ? 'f' : 'b'); // front face for 65 and back face for 67, these are detids for them
-    std::string opfilename = "asset/image/lintel/plot-with-rate-"+id+"-"+std::to_string(detid)+"-"+strategy+"-ring-"+std::to_string(ring)+".pdf";
+    double integral = fill_hist2d(xyhist,get_xy,lintels,lookup,electron_at_lintel,weight=="yes");
+    double all_lintel_rate = (integral/nof)/1e9; // 14 because this is only half of lintel and there are 7 lintels with make 14 halves
+    std::cout<<"Rate :: "<<all_lintel_rate<<std::endl;
+    update_histogram(xyhist,-850,-150,0,150,0,1e7);
+    utl::show_text(Form("Rate = %2.3f GHz ",all_lintel_rate),0.8,0.825,0.03);
+    add_lintel_outline(c1,detid == 65 ? 'f' : 'b'); // front face for 65 and back fece for 67, these are detids for them
+    std::string opfilename = "asset/image/lintel/plot-DC-RATE-vmin-vmax-"+id+"-"+std::to_string(detid)+"-"+strategy+"-ring-"+std::to_string(ring)+".pdf";
+    //std::string opfilename = "asset/image/lintel/geometry/one-slice-full-plot-without-lintel.pdf";
     c1->SaveAs(opfilename.c_str());
 }
 
@@ -99,11 +126,11 @@ int main(){
     mystyle()
     TCanvas* c1 = new TCanvas("c1","c1",800,600);
     std::string ofilename="oh-hello.txt";
-    std::string basedir = "/home/pranphy/simana/sim/output/no-lintel-20230206-155101/primary/";
+    std::string basedir = "/home/pranphy/simana/sim/output/yes-lintel-20230206-155101/primary/";
     std::string filelist = basedir+"filelist.txt";
-    int detid = 65;
+    int detid = 67;
     std::string strategy = "normal";
-    std::string weight = "no";
+    std::string weight = "yes";
     std::string id = "repl-bata";
     int ring = 5
     lintel_reverse(filelist,detid,ring,weight,strategy,id);
--- analysis/script/utils.hh
+++ analysis/script/utils.hh
@@ -5,10 +5,13 @@
 #include <vector>
 #include <functional>
 
+
 #include "TChain.h"
 #include "TTree.h"
 #include "TH2D.h"
 #include "TLatex.h"
+#include "TCanvas.h"
+#include "TPolyLine.h"
 
 typedef remollGenericDetectorHit_t RemollHit;
 typedef std::vector<RemollHit> hit_list;
@@ -40,8 +43,23 @@ namespace DETID {
     const int MD = 28;
 }
 
-
 namespace utl{
+
+// Remoll/Moller Specific
+
+bool md_ring_cut(RemollHit hit, int ring = 0){
+    int rmin = 0; int rmax = 0; int offset = 10; // Recheck offset
+    if(ring == 0){ rmin = 650; rmax = 1160; } // ring = 0 means no ring cut
+    else if(ring == 1) {rmin = 650; rmax = 680;} // Table from  DocDB 896-v1
+    else if(ring == 2) {rmin = 680; rmax = 740; }
+    else if(ring == 3) {rmin = 740; rmax = 800; }
+    else if(ring == 4) {rmin = 800; rmax = 920; }
+    else if(ring == 5) {rmin = 920; rmax = 1060; }
+    else if(ring == 6) {rmin = 1060; rmax = 1160; }
+    return (hit.r > rmin+offset) && (hit.r <= rmax+offset);
+}
+
+
 
 template<typename T, int trid>
 bool trid_cut(T obj) { return obj.trid == trid; }
@@ -55,7 +73,7 @@ bool det_pid_cut(T obj,int det, int pid) {
     return obj.det == det && (obj.pid == pid || obj.pid == -pid);
 }
 
-TChain* make_chain(std::string filelist,int nof = 0,bool verbose=false){
+TChain* make_chain(std::string filelist,int nof = 0, bool verbose=false){
     std::ifstream flist(filelist);
     std::string filename;
     TChain* TC = new TChain("T");
@@ -70,83 +88,58 @@ TChain* make_chain(std::string filelist,int nof = 0,bool verbose=false){
 }
 
 
-double fill_hist2d(TH2D* hist2d, pair_func get_xy, TTree* T, std::function<hit_list(hit_list)> lookup, std::function<bool(RemollHit)> cut, bool rateweight=false) {
-    hit_list  *hits=0; double rate;
-    T->SetBranchAddress("hit",&hits);
-    T->SetBranchAddress("rate",&rate);
-    int totentry = T->GetEntries();
-    double calc_rate = 0;
-    for(int entry = 0; entry <= totentry; ++entry) {
-        T->GetEntry(entry);
-        //if(rate > 1e10) continue; // seems to me that 1e11 is the bogus rate value.
-        hit_list rev_hits = lookup(*hits);
-        if(!rateweight) rate = 1;
-        bool flag = false;
-        for(RemollHit hit: rev_hits ) {
-            auto xyp = get_xy(hit);
-            double x = xyp.first; double y = xyp.second;
-            if( cut(hit) ) {
-                flag = true;
-                calc_rate += rate;
-                hist2d->Fill(x,y,rate);
-            }
-        }
-        //if(flag) calc_rate += rate;
-    }
-    return calc_rate;
-}
-
-double to_degree(double a) { return a*180/3.141592653589;}
-
-template <typename T> bool has_pid(std::vector<T> hits, int pid) {
+template <typename T>
+bool has_pid(std::vector<T> hits, int pid) {
     return hits.end() != std::find_if(hits.begin(),hits.end(),[&](RemollHit hit)->bool{ return hit.pid == pid;});
 }
+
+
 bool has_electron(hit_list hits) { return has_pid(hits,PID::ELECTRON) || has_pid(hits,PID::POSITRON); }
 bool has_proton(hit_list hits) { return has_pid(hits,PID::PROTON) ; }
 
-void show_text(std::string text, Double_t normX, Double_t normY, Double_t size = 0.04) {
+
+/// Graphics stuffs
+
+void make_rectangle(TCanvas* canvas,std::vector<float>& box,int color=kRed, int thickness=2,int style=2){
+    std::vector<double> x={box[0],box[0],box[1],box[1],box[0]};
+    std::vector<double> y={box[2],box[3],box[3],box[2],box[2],};
+    TPolyLine* poly = new TPolyLine(x.size(),x.data(),y.data());
+    poly->SetLineWidth(thickness); poly->SetLineColor(color); poly->SetLineStyle(style); poly->Draw();
+    canvas->Update();
+}
+
+void show_text(std::string text, Double_t normX, Double_t normY, int color=kRed, Double_t size = 0.04) {
   TLatex* label = new TLatex();
   label->SetNDC(kTRUE);
   label->SetTextAlign(22);
   label->SetTextSize(size);
-  label->SetTextColor(kRed);
+  label->SetTextColor(color);
   label->DrawLatex(normX, normY, text.c_str());
 }
 
+// C++ Helpers
 
-
-void save_hist2d(TH2D* xyhist,std::string filenameop){
-    //TH2F *hist = new TH2F("hist", "Example Histogram", 10, 0, 10, 10, 0, 10);
-    //std::string filenameop = "test-hist2d-save.txt"
-    ofstream of(filenameop,std::ios::out);
-    of << "Test Here"<<std::endl;
-    //
-    TH2D* hist = xyhist;
-    //
-    int xbins = hist->GetNbinsX();
-    int ybins = hist->GetNbinsX();
-    //
-    of<<"XBins : ";
-    for (int i = 0; i <= xbins+1; ++i) { // +1 cuz
-        float be = hist->GetXaxis()->GetBinLowEdge(i);
-        std::cout <<be << ", ";
-        of << be<<", ";
-    }
-    of<<std::endl<<"YBins : ";
-    for (int i = 0; i <= ybins+1; ++i) { // +1 cuz
-        float be = hist->GetYaxis()->GetBinLowEdge(i);
-        std::cout <<be << ", ";
-        of << be<<", ";
-    }
-    //
-    of<<std::endl << "Bin values : "<<std::endl;
-    for(int i = 0; i < xbins; ++i){
-        for(int j = 0; j < ybins; ++j) {
-            double val = hist->GetBinContent(i,j);
-            of << val << ", ";
-        }
-        of<<std::endl;
-    }
+template <typename T>
+bool contains(std::vector<T>& container,const T& elem) {
+    return container.end() != std::find(container.begin(),container.end(),elem);
 }
 
+template <typename T>
+void print(std::vector<T> container, std::string formatter="%8.1f, "){
+    printf("[ ");
+    std::for_each(container.begin(),container.end(),[&](T c){ printf(formatter.c_str(),c);});
+    printf("] ");
+}
+
+template <typename T>
+void println(std::vector<T> container, std::string formatter="%8.1f, "){
+    print(container,formatter);
+    printf("\n");
+}
+
+// General utilities
+
+double to_degree(double a) { return a*180/3.141592653589;}
+
 } // utl::
+
