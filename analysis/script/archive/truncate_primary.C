1cb8155e01b7d615c9af03930bd383897cabb17b
Prakash <fku2vr@virginia.edu>
2023-04-05 19:17:24 -0400

    Modernize some scripts

--- analysis/script/archive/truncate_primary.C
+++ analysis/script/archive/truncate_primary.C
@@ -4,12 +4,45 @@
 #include "RemollData.hpp"
 
 
-void truncate_primary(std::string primaryfile,std::string truncatedfile,unsigned count)
+void truncate_primary_og(std::string primaryfile,std::string truncatedfile,unsigned count)
 {
   RemollData primary = RemollData(primaryfile,"skim tree",RemIO::READ);
   RemollData truncated = RemollData(truncatedfile.c_str(),"T",RemIO::WRITE);
   // get firt `count` entries from the primary tree
   std::vector<RemollHit> rh = primary.get_values<RemollHit>("hit",identity<RemollHit>,count);
   truncated.add_branch<RemollHit>(rh,"hit");
+}
+
+RemollHit modify_hit(RemollHit oldhit)
+{
+    RemollHit newhit  = oldhit;
+    newhit.x =  oldhit.x;
+    newhit.y =  oldhit.y;
+    newhit.z =  oldhit.z-0.1; // 0.1mm subtracted
+    return newhit;
+}
+
+ostream& operator<<(ostream& os, RemollHit& hit)
+{
+    os<<"("<<hit.x<<","<<hit.y<<","<<hit.z<<")";
+    return os;
+}
+
+void truncate_primary(std::string primaryfile,std::string truncatedfile,unsigned count) {
+  RemollData primary = RemollData(primaryfile,"skim tree",RemIO::READ);
+  RemollData truncated = RemollData(truncatedfile.c_str(),"T",RemIO::WRITE);
+  // get firt `count` entries from the primary tree
+  std::vector<RemollHit> rh = primary.get_values<RemollHit>("hit",identity<RemollHit>,count);
+  std::cout<<"The hit object "<<std::endl;
+  for(RemollHit obj: rh)
+  {
+     //std::cout<<"(x,y,z) = ("<<obj.x<<","<<obj.y<<","<<obj.z<<")"<<std::endl;
+     std::cout<<"(x,y,z) = "<<obj;
+     RemollHit modified = modify_hit(obj);
+     std::cout<<"--> "<<modified<<std::endl;
+     //std::vector<RemollHit> v = {modified};
+     std::vector<RemollHit> v = {obj};
+     truncated.add_branch(v,"hit");
+  }
 }
 
--- analysis/script/regions_plots.C
+++ analysis/script/regions_plots.C
@@ -1,37 +1,19 @@
+#include <string>
+#include <vector>
+#include <functional>
+
 #include "TROOT.h"
 #include "TFile.h"
 #include "TTree.h"
-#include <string>
-#include <map>
 
+#include "remolltypes.hh"
 #include "utils.hh"
 #include "misc_util.hh"
 #include "RemollTree.hpp"
 
-
-std::string directory = "sim/output/real-assymetric-202303271207/primary/skim/merged/";
-std::string filename = "events-at-least-1-epm-hit-on-all-rings.root";
-std::string primary_skim = directory+filename;
-
-auto T = utl::get_tree(primary_skim);
-RemollTree RT(T,{"hit"});
 int mdring = 0;
-
-hit_list tracks_passing(hit_list hits, std::function<bool(RemollHit)> cut){
-    std::vector<int> trids;
-    std::vector<RemollHit>  rev_hits(0);
-    for(auto hit: hits){
-        if(cut(hit)){
-            trids.push_back(hit.trid);
-        }
-    }
-    for(auto hit: hits) {
-        if(utl::contains(trids,hit.trid) ){
-            rev_hits.push_back(hit);
-        }
-    }
-    return rev_hits;
-}
+std::string imagedir = "asset/image/new-krypto/primary/charged";
+auto canvas = new TCanvas("canvas");
 
 hit_list look_atleast_one(hit_list hits, std::function<bool(RemollHit)> cut){
     hit_list empty(0);
@@ -44,13 +26,11 @@ hit_list identity(hit_list hits) { return hits; }
 bool energy_cut(RemollHit hit) { return hit.e > 1; }
 
 bool electron_hitting_md(RemollHit hit){
-    return utl::md_ring_cut(hit,mdring)  && (hit.pid == PID::ELECTRON || hit.pid == PID::POSITRON);
+    return utl::md_ring_cut(hit,mdring)  and utl::epm_cut(hit); 
 }
 
 bool vz_cut(RemollHit hit,float vzmin, float vzmax){ return hit.vz > vzmin and hit.vz <= vzmax; }
 
-std::string imagedir = "asset/image/new-krypto/primary/charged";
-auto canvas = new TCanvas("canvas");
 
 void energy_hist(TTree* T, float vzmin, float vzmax, float emax=1230,float emin=1,std::string suffix=""){
     std::string ring5 = "hit.det == 28 && hit.r > 930 && hit.r <= 1070";
@@ -64,7 +44,7 @@ void energy_hist(TTree* T, float vzmin, float vzmax, float emax=1230,float emin=
 void track_rz_hist(RemollTree& RT, float vzmin, float vzmax,std::vector<float> bins={200,0,28000,200,0,2000},std::string suffix=""){
     TH2D* rzhist = new TH2D("rzhist",Form("Intercepted by virtual planes, tracks hitting Ring %d from vz(%.0f,%.0f]; z[mm]; r[mm]",mdring,vzmin,vzmax),bins[0],bins[1],bins[2],bins[3],bins[4],bins[5]);
     auto cut = [&](RemollHit hit)->bool { return energy_cut(hit) && electron_hitting_md(hit) && vz_cut(hit,vzmin,vzmax); };
-    auto lookup = [&](hit_list hits) { return tracks_passing(hits,cut); };
+    auto lookup = [&](hit_list hits) { return utl::select_tracks(hits,cut); };
     auto fill_rz = [&](RemollHit hit) { rzhist->Fill(hit.z,utl::hypot(hit.x,hit.y)); };
     loop_tree(RT,fill_rz,lookup);
     rzhist->SetStats(kFALSE);
@@ -73,21 +53,21 @@ void track_rz_hist(RemollTree& RT, float vzmin, float vzmax,std::vector<float> b
 }
 
 
-void hit_xy_at_det(RemollTree& RT, float vzmin, float vzmax, int det,std::vector<float> bins={100,-100,100,100,-100,100},std::string suffix=""){
-    TH2D* xyhist = new TH2D("xyhist",Form("All hits on det %d  with at least 1 hit in Ring %d from vz(%.0f,%.0f]; x[mm]; y[mm]",det,mdring,vzmin,vzmax),bins[0],bins[1],bins[2],bins[3],bins[4],bins[5]);
-    auto fill_xy = [&](RemollHit hit) { if(hit.det == det && utl::hypot(hit.x,hit.y) > 10 ) xyhist->Fill(hit.x,hit.y); };
+void track_r_at_det(RemollTree& RT, float vzmin, float vzmax, int det,std::vector<float> bins={100,0,100},std::string suffix=""){
+    TH1D* rhist = new TH1D("rhist",Form("All tracks that that originate at vz(%.0f,%.0f], then intersect det %d, and go on to hit in Ring %d; r[mm]; count",vzmin,vzmax,det,mdring),bins[0],bins[1],bins[2]);
+    auto fill_r = [&](RemollHit hit) { if(hit.det == det ) rhist->Fill(hit.r); };
     auto cut = [&](RemollHit hit)->bool { return energy_cut(hit) && electron_hitting_md(hit) && vz_cut(hit,vzmin,vzmax); };
-    auto lookup = [&](hit_list hits) { return look_atleast_one(hits,cut); };
-    loop_tree(RT,fill_xy,lookup);
-    xyhist->Draw("colz");
-    canvas->SaveAs(Form("%s/all-hits-on-det-%d-with-at-least-1-hit-on-ring-%d-vz-%.0f-%.0f%s.pdf",imagedir.c_str(),det,mdring,vzmin,vzmax,suffix.c_str()));
+    auto lookup = [&](hit_list hits) { return utl::select_tracks(hits,cut); };
+    loop_tree(RT,fill_r,lookup);
+    rhist->Draw();
+    canvas->SaveAs(Form("%s/hit-r-all-tracks-that-originate-at-vz-%.0f-%.0f-and-intersect-det-%d-and-hit-ring-%d%s.png",imagedir.c_str(),vzmin,vzmax,det,mdring,suffix.c_str()));
 }
 
 void track_xy_at_det(RemollTree& RT, float vzmin, float vzmax, int det,std::vector<float> bins={100,-100,100,100,-100,100},std::string suffix=""){
     TH2D* xyhist = new TH2D("xyhist",Form("All tracks that that originate at vz(%.0f,%.0f], then intersect det %d, and go on to hit in Ring %d; x[mm]; y[mm]",vzmin,vzmax,det,mdring),bins[0],bins[1],bins[2],bins[3],bins[4],bins[5]);
     auto fill_xy = [&](RemollHit hit) { if(hit.det == det ) xyhist->Fill(hit.x,hit.y); };
     auto cut = [&](RemollHit hit)->bool { return energy_cut(hit) && electron_hitting_md(hit) && vz_cut(hit,vzmin,vzmax); };
-    auto lookup = [&](hit_list hits) { return tracks_passing(hits,cut); };
+    auto lookup = [&](hit_list hits) { return utl::select_tracks(hits,cut); };
     loop_tree(RT,fill_xy,lookup);
     xyhist->Draw("colz");
     canvas->SaveAs(Form("%s/all-tracks-that-originate-at-vz-%.0f-%.0f-and-intersect-det-%d-and-hit-ring-%d%s.pdf",imagedir.c_str(),vzmin,vzmax,det,mdring,suffix.c_str()));
@@ -97,7 +77,7 @@ void track_xy_at_det(RemollTree& RT, float vzmin, float vzmax, int det,std::vect
 void hit_vrvz_hist(RemollTree& RT, float vzmin, float vzmax,std::vector<float> bins={200,0,28000,200,0,2000},std::string suffix=""){
     TH2D* rzhist = new TH2D("rzhist",Form("Vertices of hits at Ring %d from vz(%.0f,%.0f]; vz[mm]; vr[mm]",mdring,vzmin,vzmax),bins[0],bins[1],bins[2],bins[3],bins[4],bins[5]);
     auto cut = [&](RemollHit hit)->bool { return energy_cut(hit) && electron_hitting_md(hit) && vz_cut(hit,vzmin,vzmax); };
-    auto lookup = [&](hit_list hits) { return tracks_passing(hits,cut); };
+    auto lookup = [&](hit_list hits) { return utl::select_tracks(hits,cut); };
     auto fill_rz = [&](RemollHit hit) { if(electron_hitting_md(hit)) rzhist->Fill(hit.vz,utl::hypot(hit.vx,hit.vy)); };
     //auto fill_rz = [&](RemollHit hit) {  rzhist->Fill(hit.vz,utl::hypot(hit.vx,hit.vy)); };
     loop_tree(RT,fill_rz,lookup);
@@ -106,18 +86,37 @@ void hit_vrvz_hist(RemollTree& RT, float vzmin, float vzmax,std::vector<float> b
 }
 
 
+
+void count_annulus(RemollTree& RT, int detid, float rmin, float rmax,std::vector<float> bins={100,-80,80,200,-80,80},std::string suffix=""){
+    TH2D* xyhist = new TH2D("xyhist",Form("The tracks hitting Ring %d passing through r [%.2f,%.2f) of det %d; vz[mm]; vr[mm]",mdring,rmin,rmax,detid),bins[0],bins[1],bins[2],bins[3],bins[4],bins[5]);
+    auto cut = [&](RemollHit hit)->bool { return energy_cut(hit) && electron_hitting_md(hit); };
+    auto lookup = [&](hit_list hits) { return utl::select_tracks(hits,cut); };
+    auto detannulus = [&](RemollHit hit,int detid)->bool { return hit.det == detid && hit.r > rmin && hit.r < rmax; };
+    auto fill_xy = [&](RemollHit hit) { if(detannulus(hit,detid)) xyhist->Fill(hit.x,hit.y); };
+    loop_tree(RT,fill_xy,lookup);
+    xyhist->Draw("colz");
+    canvas->SaveAs(Form("%s/rpartition-hit-at-ring-%d-from-r-%.0f-%.0f-range%s.pdf",imagedir.c_str(),mdring,rmin,rmax,suffix.c_str()));
+}
+
+void other_stuffs(RemollTree& RT){
+    for(auto detid : {44, 48, 58, 66 , 72}) utl::add_vline(msc::detloc[detid]);
+    std::vector<float> rpartition = {0,40,45,50,55,90};
+    for(int ring: {0,5}){
+        mdring = ring;
+        for(size_t i = 1; i < rpartition.size(); ++i){
+            count_annulus(RT,66,rpartition[i-1],rpartition[i]);
+        }
+    }
+}
 // Ring %d/ all Ring
-// All vz/ vz < 2000
-/*
-const std::map<int,float> zloc{{44,3224.05}, {48,4707.0}}
 
-track_rz_hist(RT,0,2000,{100,2000,14000,100,0,200}); 
-track_rz_hist(RT,-1e5,1e6,{100,2000,14000,100,0,200}); 
+void regions_plots(){
+    std::string directory = "sim/output/real-assymetric-202303271207/primary/skim/merged/";
+    std::string filename = "events-at-least-1-epm-hit-on-all-rings.root";
+    std::string primary_skim = directory+filename;
+    auto T = utl::get_tree(primary_skim);
+    RemollTree RT(T,{"hit"});
 
-track_rz_hist(RT,-10000,22000,{100,msc::detloc[44]-200,msc::detloc[72]+200,100,0,200},"-at-all-det-"); 
-*/
-
-void neo_regions_plots(){
     for(auto detid : {44, 48, 58, 66 , 72}){
         float zloc = msc::detloc[detid];
         for(int ring : {0,5}){
@@ -125,33 +124,20 @@ void neo_regions_plots(){
             for(float maxz : {2000,22000 }){
                 track_xy_at_det(RT,-10000,maxz,detid,{100,-150,150,100,-150,150});
                 track_rz_hist(RT,-10000,maxz,{100,zloc-200,12000,100,0,200},Form("-at-det-%d",detid)); 
+                track_r_at_det(RT,-10000,2000,66,{100,20,160},"-");
+                utl::add_vline(53.314);
             }
         }
     }
 }
 
-mdring = 5
-track_rz_hist(RT,-10000,22000,{100,2000,13000,100,0,200})
+void test_secondary(){
+    std::string secondary = "sim/output/real-assymetric-202303271207/secondary/epm3/IR53IA1.03e-2/skim/merged/events-at-least-1-hit-md.root";
+    auto T = utl::get_tree(secondary);
+    RemollTree RT(T,{"hit"});
 
-for(auto detid : {44, 48, 58, 66 , 72}) utl::add_vline(msc::detloc[detid]);
-
-
-T->Draw("hit.vz")
-hit_vrvz_hist(RT,5000,14500,{100,5000,13000,100,0,200})
-
-
-void energy_at_dets(RemollTree& RT, float vzmin, float vzmax, int detid , std::vector<float> bins={200,0,28000,200,0,2000},std::string suffix=""){
-    TH1D* ehist = new TH1D("ehist",Form("Intercepted by , tracks hitting %d Ring %d from vz(%.0f,%.0f]; z[mm]; r[mm]",detid,mdring,vzmin,vzmax),100,0,1200);
-    auto cut = [&](RemollHit hit)->bool { return energy_cut(hit) && electron_hitting_md(hit) && vz_cut(hit,vzmin,vzmax); };
-    auto lookup = [&](hit_list hits) { return tracks_passing(hits,cut); };
-    auto fill_rz = [&](RemollHit hit) { if(hit.det == detid) ehist->Fill(hit.e); };
-    loop_tree(RT,fill_rz,lookup);
-    ehist->Draw();
-    //ehist->SetStats(kFALSE);
-    //rzhist->Draw("colz");
-    //canvas->SaveAs(Form("%s/intercepted-by-virtual-planes-tracks-hitting-ring-%d-from-vz-%.0f-%.0f%s.pdf",imagedir.c_str(),mdring,vzmin,vzmax,suffix.c_str()));
+    mdring = 5;
+    track_xy_at_det(RT,-1e6,1e6,77,{100,-300,300,100,-300,300});
+    track_r_at_det(RT,-1e6,1e6,72);
 }
 
-mdring = 5
-
-energy_at_dets(RT,-1e9,1e9,66)
