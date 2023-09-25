6bdb732fdb4393ad41498d674ddeac25f72605bd
Prakash <fku2vr@virginia.edu>
2023-09-25 15:34:05 -0400

    Use RDataframe with parallelism

--- analysis/script/moller_rate_table.C
+++ analysis/script/moller_rate_table.C
@@ -1,74 +1,73 @@
 #include <string>
 #include <vector>
 
-#include "RemollTree.hpp"
+#include "ROOT/RDataFrame.hxx"
+#include "utils.hh"
 #include "misc_ntq.hh"
-#include "localconfig.hh"
 
-static const std::string opfile = "asset/files/root/sixab-all-info-02.root";
+static const std::string opfile = "asset/files/root/sixab-all-info-01_del.root";
 
 bool kind_cut(RemollHit hit, std::string kind){
     if(kind == "photon") return utl::cut::photon(hit);
     else return utl::cut::epm(hit);
 }
 
-
-void plot_hist(RemollTree& RT,std::string id, int ring = 0,std::string kind="epm"){
-    auto cut = [ring,kind](RemollHit hit) { return utl::cut::md_ring(hit,ring) and kind_cut(hit,kind) and utl::cut::E1(hit);};
-    auto vzhist = hst::vz(RT,cut,{-10000,30000},100,Form("Ring %d all %s; vz[mm]; Count",ring,kind.c_str())); vzhist->Draw();
-    utl::save(vzhist,"ring-"+std::to_string(ring)+"-vz-"+kind,id+"/plot",opfile);
-    delete vzhist;
+void update_counts(hit_list& hl, std::vector<std::vector<hit_cut>>& cuts,std::vector<std::vector<int>>& counts){
+    std::vector< std::vector<int> > count_cuts;
+    for(auto& hit: hl){
+        for(size_t i=0; i < cuts.size(); ++i){ // partitions
+            auto ringcuts = cuts[i];
+            for(size_t j = 0; j < ringcuts.size(); ++j){ // rings
+                auto cut = ringcuts[j];
+                if( ringcuts[j](hit) ) { ++counts[i][j]; }
+            }
+        }
+    }
 }
 
-void print_table(RemollTree& RT, std::string id, std::vector<double>& flpartition, std::string kind) {
+
+void print_table(ROOT::RDataFrame rd, std::string id, std::vector<double>& flpartition, std::string kind) {
     std::vector<int> rings={0,1,2,3,4,5,6};
-    std::vector<hit_cut> ringcuts{};
-    for(int ring : rings)
-        ringcuts.push_back( [ring,kind](RemollHit hit)->bool { return utl::cut::md_ring(hit,ring) and utl::cut::E1(hit) and kind_cut(hit,kind) and hit.pz > 0; });
-    //
-    auto count_rings = msc::get_counts(RT,ringcuts,flpartition);
-    utl::save(count_rings,"all-ring-counts-"+kind,id+"/data",opfile); //treename, data, branch,
-    //
+    std::vector<std::vector<hit_cut>> cutcuts{};
+    std::vector<std::vector<int>> vzring_counts{};
+    for(size_t i = 1; i < flpartition.size(); ++i){
+        double minvz = flpartition[i-1], maxvz = flpartition[i];
+        std::vector<hit_cut> ringcuts{};
+        std::vector<int> counts{};
+        for(int ring : rings){
+            ringcuts.push_back( [ring,kind,minvz,maxvz](RemollHit hit)->bool {
+                    return utl::cut::md_ring(hit,ring) and utl::cut::E1(hit) and kind_cut(hit,kind) and hit.pz > 0
+                    and hit.vz > minvz and hit.vz <= maxvz; 
+            });
+            counts.push_back(0);
+        }
+        cutcuts.push_back(ringcuts);
+        vzring_counts.push_back(counts);
+    }
+
+    rd.Foreach([&](hit_list hl){
+        update_counts(hl,cutcuts, vzring_counts);
+    },{"hit"});
+
+    utl::save(vzring_counts,"all-ring-counts-"+kind,id+"/data",opfile); //treename, data, branch,
+
     std::vector<std::string> titles; for(int ring: rings) titles.push_back("Ring "+std::to_string(ring));
-    msc::print_counts(count_rings,titles,1e8,flpartition);
+    msc::print_counts(vzring_counts, titles,1e8,flpartition);
 }
 
 
-void moller_rate_table(std::string filename="",std::string id="",int ir4=25,int ir6a=55,int ir6b=80,std::string field=""){
-    if(id == "") id = std::string(Form("4IR%d6A%d6B%d%s",ir4,ir6a,ir6b,field.c_str()));
-    if(filename == "") filename = cfg::sixab::filename(ir4,ir6a,ir6b,field);
+void moller_rate_table(std::string filename="",std::string id=""){
+    ROOT::EnableImplicitMT();
+    ROOT::RDataFrame d("T",filename);
+    if(id == "") id = "test-babies";
 
     printf("The filename is %s \n", filename.c_str());
-    auto T = utl::get_tree(filename);
-    RemollTree RT(T,{"hit"});
 
-
-    //std::vector<double> flpartition{-1e5,-3700,2000,8500,14500,18932,19290,22200, 35000,1e6};
-    std::vector<double> flpartition{-1e5,-3700,2000,8500,14500,22200, 35000,1e6};
+    std::vector<double> flpartition{-1e5,-3700,2000,8500,14500,18932,19290,22200, 35000,1e6};
 
     for(std::string kind : {"photon", "epm"}){
-        for(int ring : {0, 5})
-            plot_hist(RT,id,ring,kind);
-        //
         for(auto v: flpartition) utl::add_vline(v);
-        print_table(RT,id,flpartition,kind);
+        print_table(d,id,flpartition,kind);
     }
 
 }
-
-
-/* Params
-std::string id = "col2-cfg07m"
-
-std::string filename = ""; int ir4 = 26, ir6a = 55, ir6b = 80;
-std::string  field="-0.81.2-aosrt";
-std::string kind = "epm"; int ring = 5;
-std::string id = std::string(Form("4IR%d6A%d6B%d%s",ir4,ir6a,ir6b,field.c_str()));
-id
-filename = cfg::sixab::filename(ir4,ir6a,ir6b,field)
-
-std::string id="NO6AB";
-kind
-
-*/
-
