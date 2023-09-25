852e8859e2d028f4a3db8708bf7f052610859e30
Prakash <fku2vr@virginia.edu>
2023-09-25 14:07:58 -0400

    Add few more edges and organize

--- analysis/script/all_edges_background.C
+++ analysis/script/all_edges_background.C
@@ -7,6 +7,8 @@
 #include "ROOT/RDataFrame.hxx"
 
 static const std::string example_file ="/mnt/stg/data/remoll/sim/output/beam-col2-cfg08/primary/skim/merged/events-at-least-1-hit-md.root";
+static const std::string opfile_ex = "asset/files/bill_info_43_df_test.root";
+static const std::string opid = "col2-cfg08";
 
 typedef std::vector<float> Box;
 typedef std::map<std::string, Box> Boxes;
@@ -16,27 +18,32 @@ const static Boxes components{ // for each components {vz0, vz1, r0, r1}
     {"All",                     {-10000,35000.0,0.0,3000.0}},
     {"All Tight",               {-3500,25000.0,0.0,1500.0}},
     {"Collimator 1",            {100.0,600.0,10.0,50.0}},
-    {"Collimator 2",            {735.0, 849,30,120}},
-    {"Two bounce shield",       {924, 3500, 20,65}}, // For real.
-    {"Collimator 4",            {3200, 3350, 182,195}},
+    {"Collimator 2",            {625.0, 849,5,120}},
+    {"Two bounce shield",       {924, 3199.9, 20,65}}, // For real.
+    {"Collimator 4",            {3200, 3350, 0,195}},
     {"Vaccum Enclosure US End", {4600,4900, 310, 350}}, // Ryan was looking at assymetry here.
     {"Photon Scraper",          {4701.00,4819.00,20.0,155.0}},
     {"Belly Plates 1",          {4900, 5942,32,57}},
     {"Belly Plates 2",          {5945, 7000,32,57}},
+    {"Bulkheads",               {6500, 9000,350,900}},
     {"Belly Plates 3",          {7000, 8000,32,57}},
     {"Belly Plates 4",          {8000.0, 9800.0,45.0,90.0, 9801.0, 8000.0 - 1, 65.0, 70.0}},
-    {"Collimator 5",            {7554, 7772, 40, 300 }},
+    {"Collimator 5",            {7554, 7772, 57.01, 300 }},
     {"Collimator 6A",           {9550.0,9720.0,52,80.0}},
-    {"Lintel",                  {9642.49,9748.49, 475.0, 824.0}},
-    {"Collimator 6B",           {10925.0,11100.0,79.0,120.0}},
+    {"Clamp 6A",                {9600.0,9750.0,100,455.0}},
+    {"Lintel",                  {9600.49,9748.49, 475.0, 950.0}},
+    {"Clamps 6B",               {10800.0,11180.0,161.0,650}},
+    {"Collimator 6B",           {10925.0,11180.0,79.0,160.0}},
     {"Collar 1",                {11600.0,12000.00,540,760}},
     {"Vaccum Enclosure DS End", {12000, 12180, 700,1300}},
+    {"Drift Pipe",              {13600, 19010, 1240,1300}},
     {"Bellows 4 Region",        {12200, 13600, 600,1300}},
-    {"Vaccum Window",           {18400, 18750, 490, 550}},
-    {"Unnamed ",                {18550.0,18750.0,700.0,900.0}},
-    {"Bellows 5",               {18900.0,19400.0,400.0,600.0}},
-    {"Det. Beam Pipe",          {19300.0,27000.0,500.0,640.0}},
-    {"Collar 2",                {18932.0,19320.0,1009.0,1040.0}},
+    {"Vaccum Window",           {18400, 19010, 490, 1150}},
+    {"Air Box 1",               {19011.0,20500.0,800.0,1009.0}},
+    {"Air Box 2",               {19011.0,22500.0,640.0,800.0}},
+    {"Bellows 5",               {19011.0,19299.0,400.0,640.0}},
+    {"Det. Beam Pipe",          {19300.0,25000.0,500.0,640.0}},
+    {"Collar 2",                {19011.0,19320.0,1009.0,1040.0}},
     {"Quartz",                  {20500,22500,800,1250}}
 };
 
@@ -44,12 +51,15 @@ void init_histmap(const Boxes& boxes, Hist2Ds& hists){
     int i = 0;
     for(auto box : boxes){
         auto name = box.first; auto xy = box.second;
-        auto get_th2d = [&xy, &i](std::string name){ return new TH2D(Form("vzvr%02d",i++),(name+ " ; vz[mm]; vr[mm]").c_str(),100,xy[0], xy[1], 100,xy[2], xy[3]); };
-        auto get_xy  =  [&xy, &i](std::string name){ return new TH2D(Form("yx%02d",i++),(name+ " ; vx[mm]; vy[mm]").c_str(),100,-xy[3], xy[3],100,-xy[3],xy[3]); };
+        auto get_th2d = [&xy, &i](std::string name){ return new TH2D(Form("vzvr%03d",i++),(name+ " ; vz[mm]; vr[mm]").c_str(),100,xy[0], xy[1], 100,xy[2], xy[3]); };
+        auto get_xy  =  [&xy, &i](std::string name){ return new TH2D(Form("yx%03d",i++),(name+ " ; vx[mm]; vy[mm]").c_str(),100,-xy[3], xy[3],100,-xy[3],xy[3]); };
+        //auto get_exy  =  [&xy, &i](std::string name){ return new TH2D(Form("yx%03d",i++),(name+ " ; x[mm]; y[mm]").c_str(),100,-xy[3], xy[3],100,-xy[3],xy[3]); };
         hists[name + " epm"] = get_th2d(name + " $e^{\\pm}$");
         hists[name + " epmxy"] = get_xy(name + " $e^{\\pm}$xy");
+        //hists[name + " epmxye"] = get_xy(name + " $e^{\\pm}$ E");
         hists[name + " photon"] = get_th2d(name + " $\\gamma$");
         hists[name + " photonxy"] = get_xy(name + " $\\gamma$");
+        //hists[name + " photonxye"] = get_xy(name + " $\\gamma$ E");
     }
 }
 
@@ -65,23 +75,25 @@ void count_box(RemollHit hit, const Boxes& boxes, Hist2Ds& hists){
             if(utl::cut::ring5_epm_E1(hit) and hit.pz > 0){
                 hists[box.first + " epm"]->Fill(vz,vr);
                 hists[box.first + " epmxy"]->Fill(hit.vx, hit.vy);
+                //hists[box.first + " epmxye"]->Fill(hit.vx, hit.vy,hit.k);
             }
             else if (utl::cut::ring5_photon_E1(hit) and hit.pz > 0){
                 hists[box.first + " photon"]->Fill(vz,vr);
                 hists[box.first + " photonxy"]->Fill(hit.vx, hit.vy);
+                //hists[box.first + " photonxye"]->Fill(hit.vx, hit.vy, hit.k);
             }
         }
     }
 }
 
-void save_hists(Hist2Ds& hists, const std::string& id, const std::string &opfile){
+void save_hists(Hist2Ds& hists, const std::string& id, const std::string &opfile,float total=1e9){
     int i = 0;
-    std::cout<<"Name, Count, Moller Fraction"<<std::endl;
+    std::cout<<"Name, Count, z-pos, Moller Fraction"<<std::endl;
     for(auto hist: hists) {
         int count = hist.second->Integral();
         std::string name = hist.first;
-        std::cout<<hist.first<<", "<<hist.second->GetXaxis()->GetXmin()<<", "<<count<<", "<<utl::moller_fraction(count,7.609e8)*100<<" "<<std::endl;
-        utl::save(hist.second,Form("vz-vr-for-%02d",++i),id+"/plot",opfile);
+        std::cout<<hist.first<<", "<<hist.second->GetXaxis()->GetXmin()<<", "<<count<<", "<<utl::moller_fraction(count,total)*100<<" "<<std::endl;
+        utl::save(hist.second,Form("vz-vr-for-%03d",++i),id+"/plot",opfile);
     }
 }
 
@@ -105,14 +117,14 @@ void fill_hist(hit_list hl, TH1D* histe, TH1D* histp, TH2D* exc){
     }
 }
 
-void make_all_plots(std::string& filename, const std::string& id, const std::string& opfile){
+void make_all_plots(const std::string& filename, const std::string& id, const std::string& opfile,float total=1e9){
     ROOT::EnableImplicitMT();
-    ROOT::RDataFrame d("T",example_file);
+    ROOT::RDataFrame d("T",filename);
     Hist2Ds histmap;
 
-    auto the = new TH1D("vzhiste","All background $e^{\\pm}$; vz[mm]; count",200,-3000, 28000);
-    auto thp = new TH1D("vzhistp","All background $\\gamma$; vz[mm]; count",200,-3000, 28000);
-    auto exc = new TH2D("vzvrpl","All background; vz[mm]; vr[mm]", 200, -3500,25000.0,200, 0.0,1500.0);
+    auto the = new TH1D("vzhiste","All background $e^{\\pm}$; vz[mm]; count",200,0, 25000);
+    auto thp = new TH1D("vzhistp","All background $\\gamma$; vz[mm]; count",200,0, 25000);
+    auto exc = new TH2D("vzvrpl","All background; vz[mm]; vr[mm]", 200,0,25000.0,200, 0.0,1450.0);
     init_histmap(components, histmap);
 
     d.Foreach([&](hit_list hl)->void {
@@ -120,16 +132,17 @@ void make_all_plots(std::string& filename, const std::string& id, const std::str
         count_boxx(hl,components,histmap);
     },{"hit"});
 
-    save_hists(histmap,id,opfile);
+    save_hists(histmap,id,opfile,total);
     utl::save(the,"vz-for-epm",id+"/plot1d",opfile);
     utl::save(thp,"vz-for-photon",id+"/plot1d",opfile);
     utl::save(exc,"exclusion-hotspots",id+"/plot1d",opfile);
 }
 
-void bly_df(){
-    static const std::string opfile = "asset/files/bill_info_43_df_test.root";
-    std::string filename = example_file;
-    static const std::string id = "col2-cfg08";
-    make_all_plots(filename, id, opfile);
-    std::cout<<"Written :"<<opfile<<std::endl;
+void all_edges_background(std::string filename=example_file, float total=1e9, std::string opfile=opfile_ex, std::string id=opid){
+    std::cout<<"# Reading :"<<filename<<std::endl;
+    std::cout<<"# Writing :"<<opfile<<std::endl;
+    std::cout<<"# ID is   :"<<id<<std::endl;
+    std::cout<<"# Total is:"<<total<<std::endl;
+    make_all_plots(filename, id, opfile,total);
+    std::cout<<"# Written :"<<opfile<<std::endl;
 }
