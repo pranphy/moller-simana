bae55f0a3296ae8ab1ababd941f533b6b4c1efff
Prakash <fku2vr@virginia.edu>
2024-05-10 12:32:30 -0400

    Remove both custom classes and use RDataFrame only to read and write

--- analysis/script/select_subset_MD.C
+++ analysis/script/select_subset_MD.C
@@ -1,29 +1,30 @@
-#include <string>
-#include <vector>
-
-#include "RemollTree.hpp"
-#include "RemollData.hpp"
+#include "utils.hh"
+#include "ROOT/RDataFrame.hxx"
 
 bool cut(RemollHit hit){
-    return utl::md_ring_cut(hit,0) && hit.e > 1 && (hit.pid == PID::ELECTRON || hit.pid == PID::POSITRON);
+    // md_ring_cut takes hit and ring number and returns true if the hit is in that ring.
+    // epm(hit) takes a hit objects return true if its either electrom or positron e+-
+    return utl::md_ring_cut(hit,0) and hit.k > 1 and utl::cut::epm(hit);
 }
 
-
-hit_list get_subset_on_det(hit_list hits, hit_cut cut){
+hit_list get_subset_on_det(hit_list& hits) {
     std::vector<RemollHit>  rev_hits(0);
     for(auto hit: hits) if(cut(hit)) rev_hits.push_back(hit);
     return rev_hits;
 }
 
-
 void select_subset_MD(std::string inputfile, std::string outputfile,std::string extension=".root"){
-    RemollTree RT(inputfile,"T");
+    ROOT::EnableImplicitMT();
+    ROOT::RDataFrame df("T",inputfile);
     std::vector<hit_list> selected;
-    for(RT.loop_init(); RT.next();){
-        hit_list hits_on_md = get_subset_on_det(*RT.cur_hits,cut); // get subset on Moller Detector
+
+    df.Foreach([&](hit_list hl)->void {
+        hit_list hits_on_md = get_subset_on_det(hl); // get subset on Moller Detector
         if (hits_on_md.size()  > 0) selected.push_back(hits_on_md);
-    }
-    RemollData RD(outputfile+extension,"T",RemIO::WRITE);
-    RD.add_branch(selected,"hit",true); // true because we want to write each element as row.
+    },{"hit"});
+
+    ROOT::RDataFrame out(selected.size()); // creates new dtaframe to store skimmed data
+    int i = 0; auto d2 = out.Define("hit", [&](){ return selected.at(i++); }); // fills the df with the data
+    d2.Snapshot("T",outputfile+extension); // writes to the disk
 }
 
