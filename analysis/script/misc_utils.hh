db4c3045355e16c649647a746a6bc8aa1f1ffedd
Prakash <fku2vr@virginia.edu>
2024-06-07 14:40:41 -0400

    Massive new addition for generic histograms

--- analysis/script/misc_utils.hh
+++ analysis/script/misc_utils.hh
@@ -1,9 +1,5 @@
-#include <map>
-
 #include "utils.hh"
 #include "ROOT/RDataFrame.hxx"
-
-typedef ROOT::RDataFrame RDF;
 
 typedef std::function<hit_list(hit_list&)> lookup_func;
 lookup_func identity_lookup = [](hit_list& hits)->hit_list { return hits;};
@@ -22,7 +18,6 @@ void loop_tree(ROOT::RDataFrame& RD, std::function<void(RemollHit)> callback, lo
 namespace msc {
 
 std::map<int,float> detloc = {
-    {817,  319.00      },
     {44 ,  3224-0.5    },
     {45 ,  3375+0.5    },
     {48 ,  4707.5-0.5  },
@@ -44,8 +39,6 @@ std::map<int,float> detloc = {
     {80 ,  19132.69    },
     {81 ,  19282.75    },
     {28 ,  22200       },
-    {815,  14500       },
-    {816,  25170       },
 };
 
 struct component {
@@ -64,9 +57,13 @@ namespace  cut_s{
 struct cut{
     std::string cutstr;
     std::string str() { return cutstr; }
-    cut operator+(std::string pcutstr){ cutstr += " && " + pcutstr; return *this; };
-    cut operator+(cut& ct){ cutstr += " && " + ct.cutstr; return *this; };
+    cut operator+(std::string pcutstr){ return cut{cutstr + " && " + pcutstr}; }
+    cut operator=(std::string pcutstr){ cutstr = pcutstr; return *this; }
+    cut operator+(cut ct){ return cut{cutstr + " && " + ct.cutstr};}
+    operator const char*() const { return cutstr.c_str();}
 };
+std::ostream& operator<<(std::ostream& os, cut& cut){ os<<cut.cutstr; return os; };
+
 cut ring5  = cut{" (hit.det == 28 && hit.r > 930 && hit.r <= 1070) "};
 cut epm    = cut{" (hit.det == 11 || hit.pid == -11) "};
 cut photon = cut{" (hit.pid == 22 ) "};
@@ -80,7 +77,7 @@ cut ring5_photon = ring5 + photon;
 
 
 
-std::vector< std::vector<int> > get_counts(RDF& RD, std::vector<hit_cut>& cuts, std::vector<double>flpartition={-1e5,-3700,8500,14500,22200, 35000,1e6}){
+std::vector< std::vector<int> > get_counts(ROOT::RDataFrame& RD, std::vector<hit_cut>& cuts, std::vector<double>flpartition={-1e5,-3700,8500,14500,22200, 35000,1e6}){
     std::vector< std::vector<int> > count_cuts;
     for(size_t i = 1; i < flpartition.size(); ++i){
         std::vector<int> counts(cuts.size(),0);
@@ -96,7 +93,7 @@ std::vector< std::vector<int> > get_counts(RDF& RD, std::vector<hit_cut>& cuts, 
 
 
 
-std::vector<int> get_counts(RDF& RD,hit_cut cut ,std::vector<double>flpartition={-1e5,-3700,8500,14500,22200, 35000,1e6}){
+std::vector<int> get_counts(ROOT::RDataFrame& RD,hit_cut cut ,std::vector<double>flpartition={-1e5,-3700,8500,14500,22200, 35000,1e6}){
     std::vector<int> counts;
     //float total = 2029;
     for(size_t i = 1; i < flpartition.size(); ++i){
@@ -222,8 +219,49 @@ hit_cut E_cut(float E1, float E2 =  12000){
 }
 
 
-} // msc::
+bool __get_det_hit(hit_list hits, int trid, hit_cut cut, RemollHit& dethit){
+    for(auto hit : hits) if(cut(hit) and hit.trid == trid) { dethit = hit; return true; }
+    return false;
+}
 
+/**
+   Given a list of cuts and a list of hits, looks up those specific tracks that pass all the cuts.
+   It returns a vector of all those hit_list where each hit_list is the hit corresponding to each
+   cut in the cut list. 
+   \param hits The hits to look up from, this is typically for an event
+   \param cuts The list of cut function, to select those hits that pass
+   \returns a matrix of hits corresponding to the cuts
+*/
+std::vector<hit_list> lookup_tracks(hit_list hits, std::vector<hit_cut> cuts){
+    std::set<int> trids; for(auto hit: hits) if(cuts[0](hit)) trids.insert(hit.trid);
+    std::vector<hit_list> trackhits;
+    for(int trid : trids){
+        bool all = true; hit_list cuthits;
+        for(auto cut : cuts){
+            RemollHit tmphit;
+            all = all and __get_det_hit(hits, trid, cut, tmphit);
+            cuthits.push_back(tmphit);
+        }
+        if(all) trackhits.push_back(cuthits);
+    }
+    return trackhits;
+}
+
+auto rr_correlate(int det1, int det2, hit_cut basecut = utl::cut::epm){
+    return [basecut,det1,det2](hit_list& hl, TH2D* hist)->void{
+        std::vector<hit_cut> cuts{basecut};
+        std::vector<int> dets = {det1,det2}; for(auto detid : dets) cuts.push_back(detid_cut(detid));
+        std::vector<hit_list> those_tracks = lookup_tracks(hl, cuts);
+        for(hit_list& cuthits : those_tracks){
+            RemollHit h0 = cuthits[1], h1 = cuthits[2];
+            hist->Fill(h0.r, h1.r);
+        }
+    };
+}
+
+
+
+} // msc::
 
 
 namespace hst {
@@ -231,7 +269,15 @@ namespace hst {
 
 typedef std::function<double(RemollHit)> __attrib_f;
 
-TH1D* hist1d(RDF& RD, hit_cut cut,
+/**
+  ! Not intended to be used by itself but by other functions defined later.
+
+  Makes 1d histogram and populates them and returns a TH1D object. It accepts 
+  cut function and the parameter of the hit object to use and the range to 
+  make histogram. It has provision for lookup function which can be used 
+  to do things like m/track lookup etc.
+  */
+TH1D* hist1d(ROOT::RDataFrame& RD, hit_cut cut,
         __attrib_f param,
         std::vector<float> w,
         int nbins=100,
@@ -247,75 +293,75 @@ TH1D* hist1d(RDF& RD, hit_cut cut,
     return hist;
 }
 
-TH1D* r(RDF& RD, lookup_func lookup, hit_cut cut, std::vector<float> w,int nbins=100, std::string title="Radius; r[mm]; Count"){
+TH1D* r(ROOT::RDataFrame& RD, lookup_func lookup, hit_cut cut, std::vector<float> w,int nbins=100, std::string title="Radius; r[mm]; Count"){
     auto param = [&](RemollHit hit)->double { return hit.r; };
     return hist1d(RD,cut,param,w,nbins,"ehist",title,lookup);
 }
 
-TH1D* r(RDF& RD, hit_cut cut, std::vector<float> w,int nbins=100, std::string title="Radius; r[mm]; Count"){
+TH1D* r(ROOT::RDataFrame& RD, hit_cut cut, std::vector<float> w,int nbins=100, std::string title="Radius; r[mm]; Count"){
     return r(RD,identity_lookup,cut,w,nbins,title);
 }
 
 
-TH1D* r(RDF& RD, std::vector<float> w,int nbins=100, std::string title="Radius; r[mm]; Count "){
+TH1D* r(ROOT::RDataFrame& RD, std::vector<float> w,int nbins=100, std::string title="Radius; r[mm]; Count "){
     return r(RD,identity_lookup,utl::__pass,w,nbins,title);
 }
 
 
 
 
-TH1D* e(RDF& RD, lookup_func lookup, hit_cut cut, std::vector<float> w,int nbins=100, std::string title="Energy; E[MeV]; Count"){
+TH1D* e(ROOT::RDataFrame& RD, lookup_func lookup, hit_cut cut, std::vector<float> w,int nbins=100, std::string title="Energy; E[MeV]; Count"){
     auto param = [&](RemollHit hit)->double { return hit.e; };
     return hist1d(RD,cut,param,w,nbins,"ehist",title,lookup);
 }
 
 
-TH1D* e(RDF& RD, hit_cut cut, std::vector<float> w,int nbins=100, std::string title="Energy; E[MeV]; Count"){
+TH1D* e(ROOT::RDataFrame& RD, hit_cut cut, std::vector<float> w,int nbins=100, std::string title="Energy; E[MeV]; Count"){
     return e(RD,identity_lookup,cut,w,nbins,title);
 }
 
 
-TH1D* e(RDF& RD, std::vector<float> w,int nbins=100, std::string title="Energy; E[MeV]; Count "){
+TH1D* e(ROOT::RDataFrame& RD, std::vector<float> w,int nbins=100, std::string title="Energy; E[MeV]; Count "){
     return e(RD,identity_lookup,utl::__pass,w,nbins,title);
 }
 
-TH1D* vz(RDF& RD, lookup_func lookup, hit_cut cut, std::vector<float> w,int nbins=100, std::string title="Vertex positions; vz[mm]; Count"){
+TH1D* vz(ROOT::RDataFrame& RD, lookup_func lookup, hit_cut cut, std::vector<float> w,int nbins=100, std::string title="Vertex positions; vz[mm]; Count"){
     auto param = [&](RemollHit hit)->double { return hit.vz; };
     return hist1d(RD,cut,param,w,nbins,"vzhist",title,lookup);
 }
 
-TH1D* vz(RDF& RD, lookup_func lookup, std::vector<float> w,int nbins=100, std::string title="Vertex positions; vz[mm]; Count"){
+TH1D* vz(ROOT::RDataFrame& RD, lookup_func lookup, std::vector<float> w,int nbins=100, std::string title="Vertex positions; vz[mm]; Count"){
     return vz(RD,lookup,utl::__pass,w,nbins,title);
 }
 
-TH1D* vz(RDF& RD, hit_cut  cut, std::vector<float> w,int nbins=100, std::string title="Vertex positions; vz[mm]; Count"){
+TH1D* vz(ROOT::RDataFrame& RD, hit_cut  cut, std::vector<float> w,int nbins=100, std::string title="Vertex positions; vz[mm]; Count"){
     return vz(RD,identity_lookup,cut,w,nbins,title);
 }
 
 
-TH1D* vz(RDF& RD, std::vector<float> w,int nbins=100, std::string title="Vertex positions; vz[mm]; Count"){
+TH1D* vz(ROOT::RDataFrame& RD, std::vector<float> w,int nbins=100, std::string title="Vertex positions; vz[mm]; Count"){
     return vz(RD,identity_lookup,utl::__pass,w,nbins,title);
 }
 
-TH1D* angled(RDF& RD, lookup_func lookup, hit_cut cut, std::vector<float> w,int nbins=100, std::string title="Angle; angle[degree]; Count"){
+TH1D* angled(ROOT::RDataFrame& RD, lookup_func lookup, hit_cut cut, std::vector<float> w,int nbins=100, std::string title="Angle; angle[degree]; Count"){
     auto angleparam = [&](RemollHit hit){ return utl::to_degree(atan(utl::hypot(hit.px,hit.py)/hit.pz)); };
     return hist1d(RD,cut,angleparam,w,nbins,"angle",title,lookup);
 }
 
-TH1D* angled(RDF& RD, hit_cut cut, std::vector<float> w,int nbins=100, std::string title="Angle; angle[degree]; Count"){
+TH1D* angled(ROOT::RDataFrame& RD, hit_cut cut, std::vector<float> w,int nbins=100, std::string title="Angle; angle[degree]; Count"){
     return angled(RD,identity_lookup,cut,w,nbins,title);
 }
 
-TH1D* angled(RDF& RD, lookup_func lookup, std::vector<float> w,int nbins=100, std::string title="Angle; angle[degree]; Count"){
+TH1D* angled(ROOT::RDataFrame& RD, lookup_func lookup, std::vector<float> w,int nbins=100, std::string title="Angle; angle[degree]; Count"){
     return angled(RD,lookup,utl::__pass,w,nbins,title);
 }
 
-TH1D* angled(RDF& RD,  std::vector<float> w,int nbins=100, std::string title="Angle; angle[degree]; Count"){
+TH1D* angled(ROOT::RDataFrame& RD,  std::vector<float> w,int nbins=100, std::string title="Angle; angle[degree]; Count"){
     return angled(RD, identity_lookup, utl::__pass, w, nbins, title);
 }
 
 
-TH2D* hist2d(RDF& RD, hit_cut cut,
+TH2D* hist2d(ROOT::RDataFrame& RD, hit_cut cut,
         __attrib_f first, __attrib_f second,
         std::vector<float> w,
         int xbins=100, int ybins=100,
@@ -331,67 +377,192 @@ TH2D* hist2d(RDF& RD, hit_cut cut,
     return xyhist;
 }
 
-TH2D* vzvr(RDF& RD, lookup_func lookup, hit_cut cut,std::vector<float> w, int nbins=100, std::string title="vr vs vz"){
+TH2D* vzvr(ROOT::RDataFrame& RD, lookup_func lookup, hit_cut cut,std::vector<float> w, int nbins=100, std::string title="vr vs vz"){
     auto first = [](RemollHit hit)->double { return hit.vz; };
     auto second = [](RemollHit hit)->double { return utl::hypot(hit.vx,hit.vy); };
     return hist2d(RD,cut,first,second,w,nbins,nbins,"vzvrhist",title+" ;vz[mm]; vr[mm]",lookup);
 }
 
 
-TH2D* vzvr(RDF& RD, lookup_func lookup, std::vector<float> w, int nbins=100, std::string title="vr vs vz"){
+TH2D* vzvr(ROOT::RDataFrame& RD, lookup_func lookup, std::vector<float> w, int nbins=100, std::string title="vr vs vz"){
     return  vzvr(RD,lookup,utl::__pass,w,nbins,title);
 }
 
 
-TH2D* vzvr(RDF& RD, hit_cut cut, std::vector<float> w, int nbins=100, std::string title="vr vs vz"){
+TH2D* vzvr(ROOT::RDataFrame& RD, hit_cut cut, std::vector<float> w, int nbins=100, std::string title="vr vs vz"){
     return  vzvr(RD,identity_lookup,cut,w,nbins,title);
 }
 
 
-TH2D* vzvr(RDF& RD, std::vector<float> w, int nbins=100, std::string title="vr vs vz"){
+TH2D* vzvr(ROOT::RDataFrame& RD, std::vector<float> w, int nbins=100, std::string title="vr vs vz"){
     return  vzvr(RD,identity_lookup,utl::__pass,w,nbins,title);
 }
 
 
-TH2D* xy(RDF& RD,lookup_func lookup, hit_cut cut, std::vector<float> w, int nbins=100, std::string title="y vs x"){
+TH2D* xy(ROOT::RDataFrame& RD,lookup_func lookup, hit_cut cut, std::vector<float> w, int nbins=100, std::string title="y vs x"){
     auto first = [](RemollHit hit)->double { return hit.x; };
     auto second = [](RemollHit hit)->double { return hit.y; };
     return hist2d(RD,cut,first,second,w,nbins,nbins,"xyhist",title+"; x[mm]; y[mm]",lookup);
 }
 
 
-TH2D* xy(RDF& RD, lookup_func lookup, std::vector<float> w, int nbins=100, std::string title="y vs x"){
+TH2D* xy(ROOT::RDataFrame& RD, lookup_func lookup, std::vector<float> w, int nbins=100, std::string title="y vs x"){
     return xy(RD,lookup,utl::__pass,w,nbins,title);
 }
 
 
-TH2D* xy(RDF& RD, hit_cut cut, std::vector<float> w, int nbins=100, std::string title="y vs x"){
+TH2D* xy(ROOT::RDataFrame& RD, hit_cut cut, std::vector<float> w, int nbins=100, std::string title="y vs x"){
     return xy(RD,identity_lookup,cut,w,nbins,title);
 }
 
-TH2D* xy(RDF& RD, std::vector<float> w, int nbins=100, std::string title="y vs x"){
+TH2D* xy(ROOT::RDataFrame& RD, std::vector<float> w, int nbins=100, std::string title="y vs x"){
     return xy(RD,identity_lookup,utl::__pass,w,nbins,title);
 }
 
-TH2D* zr(RDF& RD, lookup_func lookup, hit_cut cut, std::vector<float> w, int nbins=100, std::string title="r vs z"){
+TH2D* zr(ROOT::RDataFrame& RD, lookup_func lookup, hit_cut cut, std::vector<float> w, int nbins=100, std::string title="r vs z"){
     auto first = [&](RemollHit hit)->double { return hit.z; };
     auto second = [&](RemollHit hit)->double { return hit.r; };
     return hist2d(RD,cut,first,second,w,nbins,nbins,"zrhist",title+"; z[mm]; r[mm]",lookup);
 }
 
 
-TH2D* zr(RDF& RD, lookup_func lookup, std::vector<float> w, int nbins=100, std::string title="r vs z"){
+TH2D* zr(ROOT::RDataFrame& RD, lookup_func lookup, std::vector<float> w, int nbins=100, std::string title="r vs z"){
     return zr(RD,lookup,utl::__pass,w,nbins,title);
 }
 
-TH2D* zr(RDF& RD, hit_cut cut, std::vector<float> w, int nbins=100, std::string title="r vs z"){
+TH2D* zr(ROOT::RDataFrame& RD, hit_cut cut, std::vector<float> w, int nbins=100, std::string title="r vs z"){
     return zr(RD,identity_lookup,cut,w,nbins,title);
 }
 
 
-TH2D* zr(RDF& RD, std::vector<float> w, int nbins=100, std::string title="r vs z"){
+TH2D* zr(ROOT::RDataFrame& RD, std::vector<float> w, int nbins=100, std::string title="r vs z"){
     return zr(RD,identity_lookup,utl::__pass,w,nbins,title);
 }
+struct hist;
+typedef std::function<void(hit_list&, TH2D*)> __cbackf;
+//typedef void(*)(hit_list&, hist&) __cbackf;
 
-}; // hist::
+struct hist{
+    std::string name;
+    lookup_func lookup;
+    hit_cut cut;
+    std::vector<__attrib_f> params;
+    std::string title;
+    std::vector<float> bins;
+    TObject* obj;
+    TH1D* h1; TH2D* h2;
+    int dim;
+    __cbackf callback; bool calls;
+
+    hist(std::string name, __cbackf callback, std::string title,  std::vector<float> bins)
+        :name(name), callback(callback), title(title), bins(bins), calls(true)
+    {
+        dim = 2;
+        init_hist();
+    }
+
+    hist(std::string name, lookup_func lookup, hit_cut cut, std::vector<__attrib_f> params, std::string title,  std::vector<float> bins)
+        :name(name), lookup(lookup), cut(cut), title(title), params(params), bins(bins),  calls(false)
+    {
+        dim = params.size();
+        init_hist();
+    }
+
+    hist(std::string name, lookup_func lookup, hit_cut cut, __attrib_f param, std::string title,  std::vector<float> bins)
+        :name(name), lookup(lookup), cut(cut), title(title), params({param}), bins(bins), calls(false)
+    {
+        dim = params.size();
+        init_hist();
+    }
+
+    hist(std::string name, hit_cut cut, __attrib_f param, std::string title,  std::vector<float> bins)
+        :name(name), cut(cut), title(title), params({param}), bins(bins),  calls(false)
+    {
+        dim = params.size();
+        init_hist();
+        lookup = identity_lookup;
+    }
+    hist(std::string name, hit_cut cut, std::vector<__attrib_f> params, std::string title,  std::vector<float> bins)
+        :name(name), cut(cut), title(title), params(params), bins(bins), calls(false)
+    {
+        dim = params.size();
+        lookup = identity_lookup;
+        init_hist();
+    }
+    void init_hist(){
+        if(dim == 1)
+            h1 = new TH1D(name.c_str(),title.c_str(), bins[0],bins[1],bins[2]);
+
+        if(dim == 2)
+            h2 = new TH2D(name.c_str(),title.c_str(), bins[0],bins[1],bins[2],bins[3],bins[4],bins[5]);
+    }
+    void fill(RemollHit hit){
+        if(dim == 1){
+            //TH1D* hist = (TH1D*) obj;
+            h1->Fill(params[0](hit));
+        }
+        else if (dim == 2){
+            //TH2D* hist = (TH2D*) obj;
+            h2->Fill(params[0](hit), params[1](hit));
+        }
+    }
+    void save(std::string dir, std::string filename){
+        if(dim == 1){
+            //TH1D* hist = (TH1D*) obj;
+
+            utl::save(h1,name,dir,filename);
+        }
+        else if(dim == 2){
+            //TH2D* hist = (TH2D*) obj;
+            utl::save(h2,name,dir,filename);
+        }
+    }
+
+    void draw(std::string opt=""){
+        if(dim == 1) h1->Draw(opt.c_str()); else h2->Draw(opt.c_str());
+    }
+
+}; // hist
+
+void fill(ROOT::RDataFrame& df, std::vector<hist>& histograms){
+    auto fill_hists = [&](hit_list hits)->void {
+        for(auto& hist: histograms){
+            if(hist.calls){
+                hist.callback(hits,hist.h2);
+            } else {
+                hit_list looked_up = hist.lookup(hits);
+                for(auto hit: looked_up){
+                    if(hist.cut(hit)) hist.fill(hit);
+                }
+            }
+        }
+    };
+    df.Foreach(fill_hists,{"hit"});
+}
+
+void save(std::vector<hst::hist>& hists,std::string dir, std::string filename){
+    for(auto& hist: hists){
+        hist.save(dir,filename);
+    }
+}
+
+
+}; // hst::
+
+namespace att{
+    hst::__attrib_f x   = [](RemollHit hit){ return hit.x; };
+    hst::__attrib_f y   = [](RemollHit hit){ return hit.y; };
+    hst::__attrib_f z   = [](RemollHit hit){ return hit.z; };
+    hst::__attrib_f r   = [](RemollHit hit){ return hit.r; };
+    hst::__attrib_f e   = [](RemollHit hit){ return hit.e; };
+    hst::__attrib_f phi = [](RemollHit hit){ return utl::atan_deg(hit.y,hit.x); };
+    hst::__attrib_f th  = [](RemollHit hit){ return utl::atan_deg(std::hypot(hit.px,hit.py),hit.pz);};
+
+    hst::__attrib_f vx = [](RemollHit hit){ return hit.vx; };
+    hst::__attrib_f vy = [](RemollHit hit){ return hit.vy; };
+    hst::__attrib_f vz = [](RemollHit hit){ return hit.vz; };
+    hst::__attrib_f vr = [](RemollHit hit){ return std::hypot(hit.vx,hit.vy); };
+
+}
+
 
